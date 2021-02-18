01cc0e12db85f6c8958e4912e4914d21aa72ca1c
yokadai <76767115+yokadai@users.noreply.github.com>
2021-02-18 17:44:11 +0900

    userモデルにバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    validates :name, presence: true
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+     
 end
