72dee84bfa55d252e1ad6807b4e0181d2c019de1
yokadai <76767115+yokadai@users.noreply.github.com>
2021-02-24 17:24:38 +0900

    roomモデル単体テストコード実装

--- spec/models/room_spec.rb
+++ spec/models/room_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @room = FactoryBot.build(:room)
+  end
+
+  it "ルーム名が存在すればルーム作成できること" do
+    expect(@room).to be_valid
+  end
+
+  it "ルーム名が存在しなければルーム作成できないこと" do
+    @room.room_name = ""
+    @room.valid?
+    expect(@room.errors.full_messages).to include("Room name can't be blank")
+  end
 end
