044a5cec14996211eb801f35e94ba06a4992a245
yokadai <76767115+yokadai@users.noreply.github.com>
2021-03-01 18:12:14 +0900

    メッセージ投稿機能、単体テストコード記述

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @message = FactoryBot.build(:message)
+  end
+
+  describe 'メッセージ投稿' do
+    context "メッセージ投稿できる時" do
+      it "メッセージが存在すれば投稿できること" do
+        expect(@message).to be_valid
+      end
+    end
+
+    context "メッセージ投稿できない時" do
+      it "メッセージが存在しなければ投稿できないこと" do
+        @message.message = ""
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Message が空になっています")
+      end
+ 
+      it "roomが存在しなければ投稿できないこと" do
+        @message.room = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("Room が存在しません")
+      end
+
+      it "userが存在しなければ投稿できないこと" do
+        @message.user = nil
+        @message.valid?
+        expect(@message.errors.full_messages).to include("User が存在しません")
+      end
+    end
+  end 
 end
