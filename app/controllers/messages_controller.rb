753a3ac83b1cf3b8ae1911e76fcd00446bf64985
yokadai <76767115+yokadai@users.noreply.github.com>
2021-03-01 12:55:28 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,4 +6,20 @@ class MessagesController < ApplicationController
 
   def index2
   end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:message).merge(user_id: current_user.id)
+  end
 end
