8ca990e80021ce91e93860c984939c18b71754a8
yokadai <76767115+yokadai@users.noreply.github.com>
2021-03-15 18:15:16 +0900

    edit,updateアクションのルーティングと定義

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   resources :rooms, only: [:new, :create, :index, :destroy] do
     resources :messages, only: [:index, :create]
   end
+  resources :users, only: [:edit, :update]
 end
